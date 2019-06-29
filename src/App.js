7082854ce85a2225384116690399ad8436a3cf4a
tywi6665 <tyletcole@gmail.com>
2019-06-29 13:40:46 -0600

    installed d3 and defined constant variables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.scss';
+import * as d3 from "d3";
 import Container from './Components/Container/Container';
 
 function App() {
+
+  function solarSystem() {
+    const width = 960,
+      height = 500,
+      sunX = (width / 2) - 250,
+      sunY = (height / 2) - 150,
+      t0 = new Date().setHours(0, 0, 0, 0),
+      delta = (Date.now() - t0);
+  }
   return (
     <div className="app">
       <Container>
