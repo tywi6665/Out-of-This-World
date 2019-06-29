b82896a460767eac9702713b3343c78a045bfc8f
tywi6665 <tyletcole@gmail.com>
2019-06-29 12:53:08 -0600

    deleted boilerplate create-react-app files and added Container component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Container from './Components/Container/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <Container>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a react app!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Container>
     </div>
   );
 }
