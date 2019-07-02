64e155932059f78ac954d10e59eb5ccb641eca17
tywi6665 <tyletcole@gmail.com>
2019-07-01 19:38:37 -0600

    git orbit animation to work and styled planets

--- src/Components/Background/Background.js
+++ src/Components/Background/Background.js
@@ -4,8 +4,6 @@ import "./Background.scss";
 
 const Background = () => {
 
-    // function solarSystem() {
-
     //Defining global and constant variables
     const width = 100,
         height = 100,
@@ -17,39 +15,50 @@ const Background = () => {
     //Planetary Data 
     const planets = [
         {
-            R: 23, r: 1, speed: -1.60, phi0: 35, moons: []
+            name: "mercury",
+            R: 23, r: 1, speed: -1.60, phi0: 35,
+            moons: []
         },
         {
-            R: 45, r: 2, speed: -1.17, phi0: 185, moons: []
+            name: "venus",
+            R: 45, r: 2, speed: -1.17, phi0: 185,
+            moons: []
         },
         {
-            R: 87, r: 2, speed: -1.00, phi0: 135, moons: [   // earth
+            name: "earth",
+            R: 87, r: 2, speed: -1.00, phi0: 135,
+            moons: [
                 { R: 10, r: 1, speed: -9.00, phi0: 15 }           // the moon
             ]
         },
         {
-            R: 140, r: 1, speed: -0.80, phi0: 235, moons: [   // mars
+            name: "mars",
+            R: 140, r: 1, speed: -0.80, phi0: 235,
+            moons: [
                 { R: 6, r: 0.5, speed: -3.80, phi0: 15 },          // phobos
                 { R: 9, r: 0.5, speed: -2.80, phi0: 115 }           // deimos
             ]
         },
         {
-            R: 290, r: 15, speed: -0.43, phi0: 135, moons: [   // jupiter
-                { R: 30, r: 2, speed: -7.70, phi0: 25 },          // io
-                { R: 36, r: 1, speed: -2.45, phi0: 95 },          // europa
-                { R: 49, r: 3, speed: -1.10, phi0: 125 },          // ganymede
-                { R: 79, r: 2, speed: -0.50, phi0: 315 }           // callisto
+            name: "jupiter",
+            R: 290, r: 15, speed: -0.43, phi0: 135,
+            moons: [
+                { R: 30, r: 1.5, speed: -7.70, phi0: 25 },          // io
+                { R: 36, r: 0.8, speed: -2.45, phi0: 95 },          // europa
+                { R: 49, r: 2.5, speed: -1.10, phi0: 125 },          // ganymede
+                { R: 79, r: 1.5, speed: -0.50, phi0: 315 }           // callisto
             ]
         },
         {
-            R: 583, r: 12, speed: -0.32, phi0: 260, moons: [   // saturn
-                { R: 28, r: 1, speed: -4.10, phi0: 120 },          // mimas
-                { R: 33, r: 1, speed: -3.90, phi0: 20 },          // enceladus
-                { R: 38, r: 1, speed: -3.60, phi0: 0 },          // tethys
-                { R: 44, r: 1, speed: -3.20, phi0: 100 },          // dione
-                { R: 58, r: 2, speed: -2.90, phi0: 300 },          // rhea
-                { R: 98, r: 5, speed: -1.30, phi0: 180 },          // titan
-                { R: 188, r: 2, speed: -0.10, phi0: 10 }           // lapetus
+            name: "saturn",
+            R: 583, r: 12, speed: -0.32, phi0: 260,
+            moons: [
+                { R: 28, r: 0.8, speed: -4.10, phi0: 120 },          // mimas
+                { R: 33, r: 0.8, speed: -3.90, phi0: 20 },          // enceladus
+                { R: 38, r: 0.8, speed: -3.60, phi0: 0 },          // tethys
+                { R: 44, r: 0.8, speed: -3.20, phi0: 100 },          // dione
+                { R: 58, r: 1.5, speed: -2.90, phi0: 300 },          // rhea
+                { R: 98, r: 2.2, speed: -1.30, phi0: 180 },          // titan
             ]
         }
     ];
@@ -89,7 +98,7 @@ const Background = () => {
                 .attr("r", d.r)
                 .attr("cx", d.R)
                 .attr("cy", 0)
-                .attr("class", "planet");
+                .attr("class", function (d) { return `${d.name} planet` })
             d3.select(this)
                 .append("g")
                 .attr("transform", `translate(${d.R}, 0)`)
@@ -119,14 +128,13 @@ const Background = () => {
         });
 
     //Orbiting animation
-    // svg.selectAll(".planetary-system, .lunar-system")
-    //     .attr("transform", function (d) {
-    //         return "rotate(" + (d.phi0 + (delta * (d.speed / 100))) + ")";
-    //     });
-
-    // };
-
-    // solarSystem();
+    setInterval(() => {
+        const delta = (Date.now() - t0);
+        svg.selectAll(".planet-system, .lunar-system")
+            .attr("transform", function (d) {
+                return "rotate(" + (d.phi0 + (delta * (d.speed / 100))) + ")";
+            });
+    }, 40);
 
     return (
         <div className="background"></div>
