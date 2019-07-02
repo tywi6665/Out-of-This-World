91b953b6d79e144e8752deae4e0bdb5b62dd094b
tywi6665 <tyletcole@gmail.com>
2019-07-01 18:46:58 -0600

    added moons to the solar system

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-import * as d3 from "d3";
 import Container from './Components/Container/Container';
 import Background from './Components/Background/Background';
 
