f6933bb4ade047408d17b9735ec27339230101f6
tywi6665 <tyletcole@gmail.com>
2019-07-03 15:50:10 -0600

    added click event to Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,10 +2,41 @@ import React from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
+
+    let isOpen = false;
+
+    function Nav() {
+        const button = document.getElementById("nav-button");
+        const nav = document.getElementById("nav");
+        const overlay = document.getElementById("overlay");
+
+        isOpen ? closeNav(button, nav, overlay) : openNav(button, nav, overlay)
+    }
+
+    function openNav(button, nav, overlay) {
+        isOpen = true;
+        button.innerHTML = "-";
+        nav.classList.add("opened-nav");
+        overlay.classList.add("on-overlay");
+        overlay.addEventListener("click", () => closeNav(button, nav, overlay));
+    };
+
+    function closeNav(button, nav, overlay) {
+        isOpen = false;
+        button.innerHTML = "+";
+        nav.classList.remove("opened-nav");
+        overlay.classList.remove("on-overlay");
+        overlay.removeEventListener("click", closeNav);
+    }
+
     return (
         <>
-            <button className="nav-button">+</button>
-            <nav className="nav-container">
+            <button
+                className="nav-button"
+                id="nav-button"
+                onClick={Nav}
+            >+</button>
+            <nav className="nav-container" id="nav">
                 <ul>
                     <li><a href="#"></a></li>
                     <li><a href="#"></a></li>
@@ -14,7 +45,7 @@ const Navbar = () => {
                     <li><a href="#"></a></li>
                 </ul>
             </nav>
-            <div className="nav-overlay"></div>
+            <div className="nav-overlay" id="overlay"></div>
         </>
     );
 };
