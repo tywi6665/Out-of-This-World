a2a58ee25ce053fad34bd495515b96a676dd763b
tywi6665 <tyletcole@gmail.com>
2019-07-03 11:36:41 -0600

    building out Navbar component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,7 +3,17 @@ import "./Navbar.scss";
 
 const Navbar = () => {
     return (
-
+        <button className="nav-button">+</button>
+        <nav className="nav-container">
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </nav>
+        <div className="nav-overlay"></div>
     );
 };
 
