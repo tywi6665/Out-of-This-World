c875fbebb378a2cb684458739c679388f9ce9163
tywi6665 <tyletcole@gmail.com>
2019-07-03 14:45:55 -0600

    styling Navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.scss';
 import Container from './Components/Container/Container';
 import Background from './Components/Background/Background';
 import Modal from "./Components/Modal";
+import Navbar from "./Components/Navbar";
 
 function App() {
 
   return (
     <div className="app">
+      <Navbar />
       <Background />
       <Container>
         <Modal>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,17 +3,19 @@ import "./Navbar.scss";
 
 const Navbar = () => {
     return (
-        <button className="nav-button">+</button>
-        <nav className="nav-container">
-            <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-            </ul>
-        </nav>
-        <div className="nav-overlay"></div>
+        <>
+            <button className="nav-button">+</button>
+            <nav className="nav-container">
+                <ul>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </nav>
+            <div className="nav-overlay"></div>
+        </>
     );
 };
 
