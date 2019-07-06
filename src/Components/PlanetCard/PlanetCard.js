3b4038f364e2d127d9bd87d9b8013fe95e7fbf34
tywi6665 <tyletcole@gmail.com>
2019-07-06 10:07:33 -0600

    created planetcard component

---
+++ src/Components/PlanetCard/PlanetCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as d3 from "d3";
+
+const PlanetCard = () => {
+    return (  );
+}
+
+export default PlanetCard;
--- src/Pages/InnerPlanets/InnerPlanets.js
+++ src/Pages/InnerPlanets/InnerPlanets.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./InnerPlanets.scss";
 import Container from "../../Components/Container";
 import * as d3 from "d3";
 
 const InnerPlanets = () => {
+
+    const [innerPlanets, setInnerPlanets] = useState([
+        { name: "Mercury", tilt: 0.03, radius: 2439.7, period: 58.65, colours: ["#e7e8ec", "#b1adad"] },
+        { name: "Venus", tilt: 2.64, radius: 6051.8, period: -243, colours: ["#f8e2b0", "#d3a567"] },
+        { name: "Earth", tilt: 23.44, radius: 6371, period: 1, colours: ["#9fc164", "#6b93d6"] },
+        { name: "Mars", tilt: 6.68, radius: 3389.5, period: 1.03, colours: ["#ef1501", "#ad0000"] }
+    ])
 
     d3.select(".svg-body").remove();
 
