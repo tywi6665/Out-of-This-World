04b9600cf8bd96396cf1aa112c4028bc13b316b3
tywi6665 <tyletcole@gmail.com>
2019-07-06 10:55:51 -0600

    creating planet cards with d3

--- src/Components/PlanetCard/PlanetCard.js
+++ src/Components/PlanetCard/PlanetCard.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import "./PlanetCard.scss";
 import * as d3 from "d3";
+import { transform } from '@babel/core';
 
 const PlanetCard = ({ planet }) => {
+
+    const width = 100,
+        height = 100;
+
+    const svg = d3.select(`.planet-card-${planet.name}`)
+        .append("svg")
+        .attr("width", `${width}%`)
+        .attr("height", `${height}%`)
+        .attr("class", `svg-${planet.name}`);
+
+    const boundingSize = width - 10;
+    const boundingArea = svg.append("g")
+        .select("g")
+        .data(planet)
+        .enter()
+        .append("g")
+        .attr("transform", (d, i) => {
+            return `translate(${[i * (boundingSize + 10), height / 2]})`
+        })
+    // .on("mouseover", showInfo)
+    // .on("mouseout", hideInfo);
+
     return (
         <div className={`planet-card planet-card-${planet.name}`}>
             {planet.name}
--- src/Pages/InnerPlanets/InnerPlanets.js
+++ src/Pages/InnerPlanets/InnerPlanets.js
@@ -7,10 +7,10 @@ import * as d3 from "d3";
 const InnerPlanets = () => {
 
     const [innerPlanets, setInnerPlanets] = useState([
-        { name: "mercury", tilt: 0.03, radius: 2439.7, period: 58.65, colours: ["#e7e8ec", "#b1adad"] },
-        { name: "venus", tilt: 2.64, radius: 6051.8, period: -243, colours: ["#f8e2b0", "#d3a567"] },
-        { name: "earth", tilt: 23.44, radius: 6371, period: 1, colours: ["#9fc164", "#6b93d6"] },
-        { name: "mars", tilt: 6.68, radius: 3389.5, period: 1.03, colours: ["#ef1501", "#ad0000"] }
+        { name: "mercury", tilt: 0.03, radius: 2439.7, period: 58.65, colors: ["#e7e8ec", "#b1adad"] },
+        { name: "venus", tilt: 2.64, radius: 6051.8, period: -243, colors: ["#f8e2b0", "#d3a567"] },
+        { name: "earth", tilt: 23.44, radius: 6371, period: 1, colors: ["#9fc164", "#6b93d6"] },
+        { name: "mars", tilt: 6.68, radius: 3389.5, period: 1.03, colors: ["#ef1501", "#ad0000"] }
     ])
 
     d3.select(".svg-body").remove();
