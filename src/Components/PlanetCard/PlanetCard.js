246e47841f50e1b15a47169184509683192c30ef
tywi6665 <tyletcole@gmail.com>
2019-07-06 10:24:20 -0600

    building out InnerPlanets page

--- src/Components/PlanetCard/PlanetCard.js
+++ src/Components/PlanetCard/PlanetCard.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import * as d3 from "d3";
 
-const PlanetCard = () => {
-    return (  );
+const PlanetCard = ({ planet }) => {
+    return (
+        <div className={`planet-card planet-card-${planet.name}`}>
+            {planet.name}
+        </div>
+    );
 }
 
 export default PlanetCard;
