f206b0ed9495c0f716a640e8bfcecbddb28c6dd3
tywi6665 <tyletcole@gmail.com>
2019-07-16 14:32:47 -0600

    converted innerplanets data into json object

--- src/Pages/InnerPlanets/InnerPlanets.js
+++ src/Pages/InnerPlanets/InnerPlanets.js
@@ -1,30 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import "./InnerPlanets.scss";
 import Container from "../../Components/Container";
 import PlanetCard from "../../Components/PlanetCard";
 import * as d3 from "d3";
+const innerPlanetsData = require("../../Data/innerPlanetsData.json");
 
 const InnerPlanets = () => {
 
-    const [innerPlanets, setInnerPlanets] = useState([
-        { name: "mercury", tilt: 0.03, radius: 2439.7, period: 58.65, colors: ["#e7e8ec", "#b1adad"], moons: [] },
-        { name: "venus", tilt: 2.64, radius: 6051.8, period: -243, colors: ["#f8e2b0", "#d3a567"], moons: [] },
-        {
-            name: "earth", tilt: 23.44, radius: 6371, period: 1, colors: ["#9fc164", "#6b93d6"],
-            moons: [
-                { name: "the-moon", radius: 1736, orbitalDistance: 385000, colors: ["grey", "lightgrey"], url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/1280px-FullMoon2010.jpg" }
-            ]
-        },
-        {
-            name: "mars", tilt: 6.68, radius: 3389.5, period: 1.03, colors: ["#ef1501", "#ad0000"],
-            moons: [
-                { name: "phobos", radius: 110, orbitalDistance: 9350, colors: ["grey"], url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Phobos_colour_2008.jpg/1024px-Phobos_colour_2008.jpg" },
-                { name: "deimos", radius: 60, orbitalDistance: 23460, colors: ["grey"], url: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Deimos-MRO.jpg" }
-            ]
-        }
-    ])
-
     d3.select(".svg-body").remove();
+
+    const innerPlanets = innerPlanetsData.data
 
     return (
         <Container
@@ -36,6 +21,7 @@ const InnerPlanets = () => {
                 <div className="star-group3"></div>
             </div>
             {innerPlanets.map(innerPlanet => {
+                console.log(innerPlanet)
                 return (
                     <PlanetCard
                         planet={innerPlanet}
