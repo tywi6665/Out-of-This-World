4b82c3d0e148bc83387009ffd07c4c71802c6a82
tywi6665 <tyletcole@gmail.com>
2019-07-16 13:55:00 -0600

    stlying tool tips

--- src/Components/PlanetCard/PlanetCard.js
+++ src/Components/PlanetCard/PlanetCard.js
@@ -150,8 +150,17 @@ const PlanetCard = ({ planet, page }) => {
                     .duration(200)
                     .style("opacity", 1);
 
-                div.html(`<img src=${d.url} /><h4>${d.name.charAt(0).toUpperCase() + d.name.slice(1)}</h4>`)
-                    .style("left", `${d3.event.pageX - 116}px`)
+                div.html(`
+                        <img src=${d.url} />
+                        <div>
+                            <h4>${d.name.charAt(0).toUpperCase() + d.name.slice(1)}</h4>
+                            <ul>
+                                <li><b>Mean Radius:</b> ${d.radius}km</li>
+                                <li><b>Mean Orbital Distance:</b> ${d.orbitalDistance}km</li>
+                                <li><b>Something</b></li>
+                            </ul>
+                        </div>`)
+                    .style("left", `${d3.event.pageX - 200}px`)
                     .style("top", `${d3.event.pageY + 15}px`)
             }
 
