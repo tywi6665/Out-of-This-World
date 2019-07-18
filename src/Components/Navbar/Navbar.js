82196f37e122395c2b48020784fa9f2e820569c8
tywi6665 <tyletcole@gmail.com>
2019-07-18 09:32:44 -0600

    added ul eventlistner to nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,24 +11,27 @@ const Navbar = () => {
         const button = document.getElementById("nav-button");
         const nav = document.getElementById("nav");
         const overlay = document.getElementById("overlay");
+        const ul = document.getElementById("ul");
 
-        isOpen ? closeNav(button, nav, overlay) : openNav(button, nav, overlay)
+        isOpen ? closeNav(button, nav, overlay, ul) : openNav(button, nav, overlay, ul)
     }
 
-    function openNav(button, nav, overlay) {
+    function openNav(button, nav, overlay, ul) {
         setIsOpen(true);
         button.innerHTML = "-";
         nav.classList.add("opened-nav");
         overlay.classList.add("on-overlay");
-        overlay.addEventListener("click", () => closeNav(button, nav, overlay));
+        overlay.addEventListener("click", () => closeNav(button, nav, overlay, ul));
+        ul.addEventListener("click", () => closeNav(button, nav, overlay, ul));
     };
 
-    function closeNav(button, nav, overlay) {
+    function closeNav(button, nav, overlay, ul) {
         setIsOpen(false);
         button.innerHTML = "+";
         nav.classList.remove("opened-nav");
         overlay.classList.remove("on-overlay");
         overlay.removeEventListener("click", closeNav);
+        ul.removeEventListener("click", closeNav);
     }
 
     return (
@@ -39,7 +42,7 @@ const Navbar = () => {
                 onClick={Nav}
             >+</button>
             <nav className="nav-container" id="nav">
-                <ul>
+                <ul id="ul">
                     <li><Link to="/home"><span className="nav-item nav-item-home">Home</span></Link></li>
                     <li><Link to="/innerplanets"><span className="nav-item nav-item-inner">Inner</span></Link></li>
                     <li><Link to="/outerplanets"><span className="nav-item nav-item-outer">Outer</span></Link></li>
