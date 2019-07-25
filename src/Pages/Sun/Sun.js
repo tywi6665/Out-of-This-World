aa2f497c504eddcb5b27ae77e7d6f4964bc248b4
tywi6665 <tyletcole@gmail.com>
2019-07-24 19:34:10 -0600

    added useEffect hook to put simple sun on the sun page

--- src/Pages/Sun/Sun.js
+++ src/Pages/Sun/Sun.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from "../../Components/Container";
+import Stars from "../../Components/Stars";
 import * as d3 from "d3";
 
 
@@ -7,16 +8,27 @@ const Sun = () => {
 
     d3.select(".svg-body").remove();
 
+    useEffect(() => {
+        const svg = d3.select("#svg-sun")
+            .append("g")
+            .attr("transform", "translate(150, 150)")
+            .append("circle")
+            .attr("r", 100)
+            .style("fill", "yellow");
+    }, []);
+
     return (
         <Container
             page={"sun"}
         >
+            <Stars />
             <div className="sun-info-left">
                 Deets
             </div>
-            <div className="svg-sun">
-                The sun goes here!!
-            </div>
+            <svg id="svg-sun"
+                width="300px"
+                height="300px"
+            ></svg>
             <div className="sun-info-right">
                 Deets
             </div>
