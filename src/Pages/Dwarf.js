420cc4fe6df748d07ba888bebf0e0b7b5ebcf3f2
tywi6665 <tyletcole@gmail.com>
2019-07-27 16:05:14 -0600

    scaled dwarf planets using scaleLinear

--- src/Pages/Dwarf.js
+++ src/Pages/Dwarf.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from "../Components/Container";
 import Stars from "../Components/Stars";
 import * as d3 from "d3";
@@ -8,36 +8,46 @@ const Dwarf = () => {
 
     d3.select(".svg-body").remove();
 
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     useEffect(() => {
 
+        d3.select(".dwarf-system").remove();
+
         const data = dwarfData.data;
-        console.log(data)
 
         const height = 400,
-            width = window.innerWidth,
-            padding = 15;
+            width = windowWidth;
 
-        const svg = d3.select("#svg-dwarf")
+        const svg = d3.select("#svg-dwarf");
 
         const radiusScale = d3.scaleLinear()
-            .domain([0, d3.extent(data, (d) => d.radius)])
-            .range([0, 400])
+            // .domain(d3.extent(data, d => d.radius))
+            .domain([0, 1188.3])
+            .range([0, 400]);
 
         const dwarfSystem = svg.append("g")
             .attr("class", "dwarf-system")
-            .attr("transform", "translate(120, 0)")
+            .attr("transform", `translate(${width * 0.15}, 0)`);
 
         dwarfSystem.selectAll("g.dwarf-planet")
             .data(data)
             .enter()
             .append("g")
             .attr("class", "dwarf-planet")
-            .attr("transform", (d, i) => "translate(" + [(i * (width / data.length)), height / 2] + ")")
+            .attr("transform", (d, i) => "translate(" + [(i * ((width - width * 0.15) / data.length)), 50] + ")")
             .append("circle")
-            .attr("r", 10)
-            .style("fill", "white")
+            .attr("class", d => `${d.name}`)
+            .attr("r", d => radiusScale(d.radius) / 10)
+            .style("fill", "white");
 
-    }, [])
+    }, [windowWidth]);
 
     return (
         <Container
@@ -47,7 +57,7 @@ const Dwarf = () => {
             <h4>{dwarfData.definition}</h4>
             <svg
                 id="svg-dwarf"
-                width="100%"
+                width={windowWidth - 20}
                 height="400px"
             ></svg>
         </Container>
