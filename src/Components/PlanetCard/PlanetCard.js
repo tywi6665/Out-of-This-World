768c82558fd855e1f933530acae9e4847ec3136e
tywi6665 <tyletcole@gmail.com>
2019-08-01 10:44:43 -0600

    created ne PlanetCard Component

---
+++ src/Components/PlanetCard/PlanetCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./PlanetCard.scss";
+
+const PlanetCard = () => {
+    return (
+        <p>this is going to be a cool planet card</p>
+    );
+}
+
+export default PlanetCard;
