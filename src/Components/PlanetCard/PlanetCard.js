b8d1f926939c6f655a182c64cd9721276be6ea57
tywi6665 <tyletcole@gmail.com>
2019-08-01 11:26:44 -0600

    adding event listners to card

--- src/Components/PlanetCard/PlanetCard.js
+++ src/Components/PlanetCard/PlanetCard.js
@@ -2,20 +2,39 @@ import React from 'react';
 import "./PlanetCard.scss";
 
 const PlanetCard = () => {
+
+    function expandCard(e) {
+        const card = document.getElementById("planet-card");
+        card.classList.add("clicked");
+        const openContent = document.getElementById("open-content");
+        openContent.classList.add("open")
+    };
+
+    function closeCard() {
+
+    };
+
     return (
         <>
-            <div class="planet-card">
-                <div class="border"></div>
+            <div id="planet-card" className="planet-card" onClick={expandCard}>
+                <div className="border"></div>
                 <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/53148/deathtostock-00.jpg" />
                 <h1>Hey now, you're an allstar</h1>
             </div>
 
-            <div id="cover" class="cover"></div>
+            <div id="cover" className="cover"></div>
 
-            <div id="open-content" class="open-content">
-                <a href="#" id="close-content" class="close-content"><span class="x-1"></span><span class="x-2"></span></a>
+            <div id="open-content" className="open-content">
+                <a href="#"
+                    id="close-content"
+                    className="close-content"
+                    onClick={closeCard}
+                >
+                    <span className="x-1"></span>
+                    <span className="x-2"></span>
+                </a>
                 <img id="open-content-image" src="" />
-                <div class="text" id="open-content-text">
+                <div className="text" id="open-content-text">
                 </div>
             </div>
         </>
