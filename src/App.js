46962bc7a45e194b85ed026038e08c813dc3cda8
tywi6665 <tyletcole@gmail.com>
2019-08-10 14:09:35 -0600

    added scroll to top npm package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.scss';
+import ScrollToTop from 'react-router-scroll-top'
 import Navbar from "./Components/Navbar";
 import Home from './Pages/Home';
 import InnerPlanets from './Pages/InnerPlanets';
@@ -13,19 +14,21 @@ function App() {
 
   return (
     <Router>
-      <div className="app">
-        <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/home" component={Home} />
-        <Route path="/innerplanets" component={InnerPlanets} />
-        <Route path="/outerplanets" component={OuterPlanets} />
-        <Route path="/sun" component={Sun} />
-        <Route path="/dwarf" component={Dwarf} />
-        <div className="disclaimer">
-          <p>*Not to Scale</p>
+      <ScrollToTop>
+        <div className="app">
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/innerplanets" component={InnerPlanets} />
+          <Route path="/outerplanets" component={OuterPlanets} />
+          <Route path="/sun" component={Sun} />
+          <Route path="/dwarf" component={Dwarf} />
+          <div className="disclaimer">
+            <p>*Not to Scale</p>
+          </div>
         </div>
-      </div>
-      <Footer />
+        <Footer />
+      </ScrollToTop>
     </Router>
   );
 }
