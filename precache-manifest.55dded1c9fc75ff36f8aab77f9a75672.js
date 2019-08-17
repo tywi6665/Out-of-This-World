e8c86b55baa67afb0179f43e6fe7aaec5853c630
tywi6665 <tyletcole@gmail.com>
2019-08-17 16:46:16 -0600

    Updates

---
+++ precache-manifest.55dded1c9fc75ff36f8aab77f9a75672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2435b9a3c224e1fbb84a91e1e128b68d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d08790639534bcc8c53",
+    "url": "/static/css/main.1b5d9a17.chunk.css"
+  },
+  {
+    "revision": "51e27d986b554e51bc85",
+    "url": "/static/js/2.fb92a894.chunk.js"
+  },
+  {
+    "revision": "9d08790639534bcc8c53",
+    "url": "/static/js/main.ade90a55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
